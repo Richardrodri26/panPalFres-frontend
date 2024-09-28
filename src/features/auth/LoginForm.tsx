0a0967161c4fca1dba57a004229e8e9453a82285
Dorance <dorancegdg@gmail.com>
2024-09-28 16:12:08 -0500

    implementacion formulario LOGIN

--- src/features/auth/LoginForm.tsx
+++ src/features/auth/LoginForm.tsx
@@ -1,9 +1,10 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { BasicFormProvider, InputForm } from "@/composables";
+import { BasicFormProvider } from "@/composables";
 import { loginSchema, loginSchemaType } from "..";
 import { Button } from "@/components/ui/button";
 import { useNavigate } from "react-router-dom";
-import { useEffect, useState } from "react";
+import { useState } from "react";
+
 
 export const LoginForm = () => {
   const [email, setEmail] = useState<string>(""); // Almacenar el email
@@ -51,60 +52,74 @@ export const LoginForm = () => {
     navigate("/registrarse");
   };
 
-  {
-    /*<input
-        placeholder="Prueba"
-        type="text"
-        value={email}
-        onChange={(event) => {
-          setEmail(event.target.value);
-        }}
-      />
-      <input
-        placeholder="password"
-        type="text"
-        value={password}
-        onChange={(event) => {
-          setPassword(event.target.value);
-        }}
-      />*/
-  }
-
-  // console.log('email', email)
-  // console.log('password', password)
-
   return (
     <BasicFormProvider submit={onSubmit} schema={loginSchema}>
-      <p className="text-[#605DEC] text-xl font-semibold">Iniciar sesión</p>
-
-      <label>corre electronico</label>
+      <h2 className="text-3xl font-semibold text-[#605DEC] p-0 text-center ">
+        LOGIN
+      </h2>
+      <p className="text-[#c7b871] text-xl font-bold mt-4">Iniciar sesión</p>
 
       {/* Campo de email */}
-      <input className="border border-red-500"
-        name="email"
-        value={email}
-        onChange={(event) => setEmail(event.target.value)}
-      />
 
-      <label>contraseña</label>
+      <div className="mt-6">
+        <label className="text-lg font-medium">email</label>
+        <input
+          className="w-full border-2 border-grey-100 rounded-xl p-4 mt-2 bg-transparent"
+          placeholder="Ingrese su correo"
+          name="email"
+          value={email}
+          pattern="[a-z0-9._%+\-]+@[a-z0-9.\-]+\.[a-z]{2,}$"
+          size={30} // En TypeScript es importante que el valor sea un número
+          minLength={4}
+          maxLength={30}
+          required
+          onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+            setEmail(event.target.value)
+          }
+        />
+      </div>
+
       {/* Campo de contraseña */}
-      <input className="border border-red-500"
-        name="password"
-        type="password"
-        value={password}
-        onChange={(event) => setPassword(event.target.value)}
-      />
+
+      <div className="mt-3">
+        <label className="text-lg font-medium">Password</label>
+        <input
+          className="w-full border-2 border-grey-100 rounded-xl p-4 mt-2 bg-transparent"
+          placeholder="Ingrese tu contraseña Mínimo 8 caracteres, al menos una letra y un número"
+          name="clave"
+          value={password}
+          type="password"
+          size={15}
+          minLength={8}
+          maxLength={15}
+          required
+          onChange={(event) => setPassword(event.target.value)}
+        />
+      </div>
 
       {/* Enlace para ir al registro */}
-      <p onClick={goToRegister} className="hover:underline cursor-pointer">
-        ¿No tienes usuario? registrate
-      </p>
+
+      <div className="mt-3">
+        <p className="py-1 text-center text-[#605DEC] text-lg font-bold hoover:scale-[1.01]" onClick={goToRegister}>
+          ¿No tienes usuario? registrate
+        </p>
+      </div>
 
       {/* Mostrar mensaje de error si existe */}
       {errorMessage && <p className="text-red-500">{errorMessage}</p>}
 
       {/* Botón para ingresar */}
-      <Button onClick={loginBackend}>INGRESAR</Button>
+
+      <div className="mt-5 flex flex-col">
+        <Button
+          onClick={loginBackend}
+          className=" active:scale-[.98] active-duration-75 hoover:scale-[1.01] ease-in-out transition-all py-2 rounded-xl bg-[#D2D2CA] text-[#c7b871] text-lg font-bold"
+        >
+          INGRESAR
+        </Button>
+      </div>
+
+    
     </BasicFormProvider>
   );
 };
