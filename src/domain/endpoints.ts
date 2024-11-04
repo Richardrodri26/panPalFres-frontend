cfc4674fb5124972fa26faa12a019587695a088c
RichardRodri26 <richardmanuel811@gmail.com>
2024-11-04 14:30:25 -0500

    adding download user report

--- src/domain/endpoints.ts
+++ src/domain/endpoints.ts
@@ -2,6 +2,7 @@
 
 export const panPalFresEndpoints = {
   LOGIN: '/auth/login',
+  USERS_REPORT: '/auth/user-report',
   PRODUCTS: '/products',
   VERIFY_USER: '/auth/check-status',
   USERS: '/auth/users',
--- src/features/users/elements/ToolbarActionsProducts.tsx
+++ src/features/users/elements/ToolbarActionsProducts.tsx
@@ -5,13 +5,22 @@ import { Button } from '@/components/ui/button'
 import { PlusCircle } from 'lucide-react'
 import { useState } from 'react'
 import { CreateUserModal } from '../modals/CreateUserModal'
+import { panPalFresEndpoints } from '@/domain/endpoints'
 
 export const ToolbarActionsUsers = () => {
-  const [isOpen, setIsOpen] = useState(false)
+  const [isOpen, setIsOpen] = useState(false);
+
+  const onDownloadUserReport = () => {
+    window.open(import.meta.env.VITE_APP_BACKEND + panPalFresEndpoints.USERS_REPORT, '_blank')
+  }
 
   return (
     <>
       <div className="py-2 w-full flex justify-end items-center gap-2">
+
+        <Button onClick={onDownloadUserReport}>
+          Descargar reporte de usuarios
+        </Button>
 
         <Button onClick={() => setIsOpen(true)} size={'sm'}>
           <PlusCircle className="h-3.5 w-3.5 mr-1" />
